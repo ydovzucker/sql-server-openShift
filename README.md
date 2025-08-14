ab0e76370586f8ee58cb73d12433fff839562694
dov zucker <ydovzucker@gmail.com>
2025-08-14 17:12:46 +0300

    last changes

---
+++ README.md
@@ -0,0 +1,4 @@
+***open shift-sql
+this project builds to images , one id=s the sql server and one is a server with acsess to query the sql server 
+**files
+*scripts/yaml files
--- scripts/commands.bat
+++ scripts/commands.bat
@@ -3,3 +3,4 @@ oc get pods
 oc rsh mysql-1-mphgp
 CREATE TABLE table_name (id INT,first_name VARCHAR(100),last_name VARCHAR(100));
 INSERT INTO table_name (id,first_name, last_name) VALUES  (101, 'Alice', 'Johnson'), (102, 'Charlie', 'Brown'), (103,'Dave','White') ,(104,'Callum','Scot');
+docker build -t my-web-app:1.0 .
